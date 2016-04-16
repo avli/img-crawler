3239228a1a98d6bc1eaeab03a0a6e3cf5844aab3
Andrey Lisin <andrey.lisin@gmail.com>
2016-04-16 17:20:04 +0600

    Save current progress

--- src/img_crawler/core.clj
+++ src/img_crawler/core.clj
@@ -1,5 +1,6 @@
 (ns img-crawler.core
-  (:require [pl.danieljanus.tagsoup :refer [parse tag attributes children]])
+  (:require [pl.danieljanus.tagsoup :refer [parse-string tag attributes children]])
+  (:require [clj-http.client :as client])
   (:gen-class))
 
 (defn img-tag? [tag]
@@ -13,6 +14,10 @@
   (when (img-tag? tag)
     (src-attribute attributes)))
 
+;; In case I want to actually crawl the page, I need to extract <a>
+;; tags as well... I think, I should rework this function. I want to
+;; have something like this: {:hrefs [...] :img-srcs [...]}
+
 (defn imgs-src
   "Takes a page's DOM, finds all <img> tags and returns
   a list of their src attributes values."
@@ -24,13 +29,38 @@
       (conj [] (get-src-if-img tag attrs))
       (filter some? (concat (conj [] (get-src-if-img tag attrs)) (mapcat imgs-src (filter vector? children)))))))
 
+(defn extract-src-or-href
+  "Depending on the tag attribute value extracts src attribute (for
+  <img>) or href (for <a>)."
+  [tag attributes]
+  (case tag
+    :img (:src attributes)
+    :a (:href attributes)
+    nil))
+
+(defn process-page
+  "Takes a page's DOM and returns a hash-map with images URLs and list
+  of <a> tags href attributes."
+  [dom]
+  (let [tag (tag dom)
+        attrs (attributes dom)
+        children (children dom)]
+    (if (or (not children) (string? children))
+      (conj [] (get-src-if-img tag attrs))
+      (filter some? (concat (conj [] (get-src-if-img tag attrs)) (mapcat imgs-src (filter vector? children)))))))
+
+(defn construct-absolute-path
+  "Constructs absolute path to the resources (image in our case)."
+  [prefix img-src]
+  (str prefix "/" img-src))
+
+;; This function should return a list of images URLs and a list of
+;; hrefs.
 (defn crawl-page [url]
-  (let [content (parse url)]
-    ;; content is a vector of vectors... I need to look a bit
-    ;; closer...  So... we have a vector of vectors represents DOM...
-    ;; And attributes are hash-maps... Our goal is to extract all
-    ;; <img> tags and their attriutes... Smells like *recursion* :)
-    ))
+  (let [h {"User-Agent" "Mozilla/5.0 (Windows NT 6.1;) Gecko/20100101 Firefox/13.0.1"}
+        resp (client/get url {:headers h})
+        dom (parse-string (:body resp))]
+    (map (partial construct-absolute-path url) (imgs-src dom))))
 
 (defn run-crawler [url]
   ;; Not sure about this function yet... Let's start with single page
@@ -40,10 +70,11 @@
 (defn -main
   "Let's the first argument will be the URL."
   [& args]
-  (if (empty? args)
+  (if (< 2 (count args))
     (do
-      (println "Please, specify the URL")
+      (println "Usage: lein run <URL> <TARGET DIR>")
       (System/exit 1))
     ;; else do the thing!
-    (let [url (first args)]
+    (let [url (first args)
+          target-dir (second args)]
       (run-crawler url))))
--- test/img_crawler/core_test.clj
+++ test/img_crawler/core_test.clj
@@ -8,4 +8,13 @@
   (testing "img-src the non-empty dom, no img tag"
     (is (= '() (imgs-src []))))
   (testing "img-src the non-empty dom has the img tag"
-    (is (= '("foo") (imgs-src [:html {} [:img {:src "foo"}]])))))
+    (is (= '("foo") (imgs-src [:html {} [:img {:src "foo"}]]))))
+  (testing "extracting src and href attributes"
+    (is (= nil (extract-src-or-href :html {:foo 42 :bar 100})))
+    (is (= nil (extract-src-or-href :img {:href 42 :bar 100})))
+    (is (= 42 (extract-src-or-href :img {:src 42 :bar 100})))
+    (is (= 100 (extract-src-or-href :a {:src 42 :href 100})))))
+
+(deftest work-with-urls
+  (testing "constructing an absolute path to an image"
+    (is "http://example.com/image.png" (construct-absolute-path "http://example.com/" "image.png"))))
