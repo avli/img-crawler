15405371ea513f02d31b76825e29c6f02d32f7b6
Andrey Lisin <andrey.lisin@gmail.com>
2016-04-17 16:53:51 +0600

    Improve pages filtering

--- src/img_crawler/core.clj
+++ src/img_crawler/core.clj
@@ -2,7 +2,7 @@
   (:gen-class)
   (:require [pl.danieljanus.tagsoup :refer [parse-string tag attributes children]])
   (:require [clj-http.client :as client])
-  (:require [clojure.string :refer [starts-with? ends-with?]])
+  (:require [clojure.string :refer [starts-with? ends-with? split]])
   (:import (java.io IOException)))
 
 (defn get-url-contents [url]
@@ -33,14 +33,20 @@
                (recur new-images new-pages (rest children)))
              [images pages])))))))
 
+(defn filter-pages [url pages]
+  (let [current-page (last (split url #"/"))
+        pred (every-pred #(not= (last (split % #"/")) current-page) #(not (starts-with? % "/#")) #(not (re-find #"#" %)))]
+    (filter pred (filter (every-pred #(not= "/" %) #(not (nil? %))) pages))))
+
 (defn process-page [url]
   (println (str "Processing page " url))
   (if-let [data (get-url-contents url)]
     (let [node (parse-string data)
           [images pages] (process-node node)]
+      (println (filter-pages url pages))
       (if-not pages
         images
-        (concat images (mapcat process-page (map #(str url %) (filter #(not (or (= % "/") (starts-with? (str %) "/#"))) pages))))))))
+        (concat images (mapcat process-page (map #(str url %) (filter-pages url pages))))))))
 
 (defn start [url target-dir]
   ;; 1. Ensure target-dir exists.
